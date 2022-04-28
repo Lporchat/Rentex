1d4bb51369638c5fb683fcfeb92c0bd6914410a0
Leonardo Vinicius <lporchat06@gmail.com>
2022-04-28 20:56:13 -0300

    modificaçao na conexão

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -1,12 +1,11 @@
 import { Connection, createConnection, getConnectionOptions } from "typeorm";
 
 //caso não seja informado o host ele deve usar o do docker, caso contrario usara o que sera informado
-export default async (host = "database"): Promise<Connection> => {
+export default async (): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
-  
+
   return createConnection(
     Object.assign(defaultOptions, {
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database:
         process.env.NODE_ENV === "test"
           ? "rentx_test"
